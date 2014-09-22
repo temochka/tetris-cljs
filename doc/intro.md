51e48106d81841e01ab5c588262389da25cf3119
Artem Chistyakov <tema@wildbit.com>
2014-09-22 19:06:14 -0400

    Remove extra directories, add temporary dirs to .gitignore.

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to tetris
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- test/tetris/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns tetris.core-test
-  (:require [clojure.test :refer :all]
-            [tetris.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
